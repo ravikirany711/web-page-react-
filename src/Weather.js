1312c890b62762475f27e082e398f7d82acbd853
ravikirany711 <ravikirany711@gmail.com>
2019-05-10 13:19:35 +0530

    basic component

---
+++ src/Weather.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Weather.css'
+
+class Weather extends Component {
+    render() {
+        return (
+            <div className='Weather'>
+                <h2>Weather</h2>
+                <p>10 C</p>
+
+            </div>
+
+        )
+    }
+}
+
+export default Weather;
